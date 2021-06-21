a31f7a861d21b6ff8c593c104dbb254b6e5afd15
agilesh-git <agileshkvk@gmail.com>
2021-06-21 14:23:34 +0530

    First Commit

---
+++ app/src/main/java/com/covid/covid19tracker/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.covid.covid19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_TIMEOUT = 2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+
+}
